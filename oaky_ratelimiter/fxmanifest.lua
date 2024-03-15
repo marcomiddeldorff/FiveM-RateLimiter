f617c2b840f45e37087d9009009cc232f22bb862
oaky <81476563+oakyydev@users.noreply.github.com>
2024-03-15 19:38:47 +0100

    chore: Removed build files from github

--- oaky_ratelimiter/fxmanifest.lua
+++
@@ -1,8 +0,0 @@
-fx_version 'bodacious'
-game 'gta5'
-
-author 'oaky'
-version '1.0.0'
-description 'Adds a rate limit functionality to your server to prevent spamming server actions'
-
-server_script 'Server/*.net.dll'
