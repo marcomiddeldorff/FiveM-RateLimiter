13048a19ef69a91188151e01bf52d01e7be0396b
oaky <81476563+oakyydev@users.noreply.github.com>
2024-03-15 19:37:58 +0100

    chore: Refactored paths and file locations

---
+++ src/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'oaky'
+version '1.0.0'
+description 'Adds a rate limit functionality to your server to prevent spamming server actions'
+
+server_script 'Server/*.net.dll'
